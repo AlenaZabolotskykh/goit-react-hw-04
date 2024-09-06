d3cdf5f7432e73b1d7a57a2f74340678c852fe5b
Alona Zabolotskykh <alenka.polishchuk@gmail.com>
2024-09-06 18:33:20 +0300

    fix-api

--- src/getImage.js
+++ src/getImage.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const KEY = "ed2sR4g0UcheyXSFHmZdb1MPIy4PjZh6mMedQmRfHO8";
+
 export const getImage = async (query, page) => {
   const { data } = await axios.get(
-    `https://api.unsplash.com/photos/?client_id=${KEY}&query=${query}&page=${page}&orientation=landscape`
+    `https://api.unsplash.com/search/photos?client_id=${KEY}&query=${query}&page=${page}&orientation=landscape`
   );
   return data;
 };
